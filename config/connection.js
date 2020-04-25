0928708d126596e19058e014a56555438311fb6d
Carolinams7 <57420037+Carolinams7@users.noreply.github.com>
2020-04-24 22:06:17 -0500

    heroku update

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,23 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  //Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //Localhost
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
